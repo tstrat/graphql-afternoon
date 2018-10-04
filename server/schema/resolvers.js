143819c5d85bf4cbaf8bba247b4d0a8230571eee
Steven Isbell <steveni_92@msn.com>
2018-10-03 20:44:06 -0500

    updated solution

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -20,26 +20,36 @@ const resolvers = {
         cartItem.quantity += 1;
       } else {
         const product = products.find(val => val.id === +id);
-        product.quantity = 1;
-        cart.push(product);
+        if (!product) {
+          throw new Error(`No Product With ID: ${id}`);
+        }
+        const productClone = {
+          ...product,
+          quantity: 1
+        };
+        cart.push(productClone);
       }
       return cart;
     },
     removeProductFromCart(_, { id }, req) {
+      const cartItem = cart.find(val => val.id === +id);
+      if (!cartItem) {
+        throw new Error(`No Item With ID: ${id}`);
+      }
       cart = cart.filter(val => val.id !== +id);
       return id;
     },
     updateQuantity(_, { id, change }) {
-      const product = cart.find(val => val.id === +id);
-      if (!product) {
-        throw new Error(`No Product Matching ID: ${id}`);
+      const cartItem = cart.find(val => val.id === +id);
+      if (!cartItem) {
+        throw new Error(`No cartItem Matching ID: ${id}`);
       }
       if (change === 'up') {
-        product.quantity += 1;
-      } else if (change === 'down' && product.quantity > 0) {
-        product.quantity -= 1;
+        cartItem.quantity += 1;
+      } else if (change === 'down' && cartItem.quantity > 0) {
+        cartItem.quantity -= 1;
       }
-      return product;
+      return cartItem;
     }
   }
 };
